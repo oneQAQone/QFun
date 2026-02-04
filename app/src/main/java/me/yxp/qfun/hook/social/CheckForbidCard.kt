9790b5aebf11d312c149f8ab5b72fb0e008f1d97
oneQAQone <3900183615@qq.com>
2026-02-04 09:34:33 +0800

    fix: 在 TIM 上查看异常资料卡不生效

--- app/src/main/java/me/yxp/qfun/hook/social/CheckForbidCard.kt
+++ app/src/main/java/me/yxp/qfun/hook/social/CheckForbidCard.kt
@@ -6,7 +6,9 @@ import me.yxp.qfun.annotation.HookCategory
 import me.yxp.qfun.annotation.HookItemAnnotation
 import me.yxp.qfun.hook.base.BaseSwitchHookItem
 import me.yxp.qfun.utils.hook.hookBefore
+import me.yxp.qfun.utils.qq.HostInfo
 import me.yxp.qfun.utils.reflect.findMethod
+import me.yxp.qfun.utils.reflect.toClass
 
 @HookItemAnnotation(
     "查看封号资料卡",
@@ -16,8 +18,11 @@ import me.yxp.qfun.utils.reflect.findMethod
 object CheckForbidCard : BaseSwitchHookItem() {
 
     override fun onHook() {
-        FriendProfileCardActivity::class.java
-            .findMethod {
+        val activity = if (HostInfo.isTIM)
+            "com.tencent.mobileqq.profilecard.activity.TimFriendProfileCardActivity".toClass
+        else FriendProfileCardActivity::class.java
+
+        activity.findMethod {
                 name = "onCardUpdate"
             }
             .hookBefore(this) { param ->
