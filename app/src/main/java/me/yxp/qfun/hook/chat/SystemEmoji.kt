6d02b856de85f25ca42748a080082eddc52aae16
oneQAQone <3900183615@qq.com>
2026-02-08 22:14:32 +0800

    chore: 修改 SystemEmoji 中的一些代码

--- app/src/main/java/me/yxp/qfun/hook/chat/SystemEmoji.kt
+++ app/src/main/java/me/yxp/qfun/hook/chat/SystemEmoji.kt
@@ -4,17 +4,17 @@ import me.yxp.qfun.annotation.HookCategory
 import me.yxp.qfun.annotation.HookItemAnnotation
 import me.yxp.qfun.hook.base.BaseSwitchHookItem
 import me.yxp.qfun.utils.hook.returnConstant
-import me.yxp.qfun.utils.reflect.clazz
 import me.yxp.qfun.utils.reflect.findMethod
+import me.yxp.qfun.utils.reflect.toClass
 
 @HookItemAnnotation(
-    tag = "强制使用系统Emoji",
-    desc = "屏蔽QQ自带的Emoji表情，强制显示系统Emoji",
-    category = HookCategory.CHAT
+    "强制使用系统Emoji",
+    "屏蔽QQ自带的Emoji表情，强制显示系统Emoji",
+    HookCategory.CHAT
 )
 object SystemEmoji : BaseSwitchHookItem() {
     override fun onHook() {
-        val target = "com.tencent.mobileqq.text.EmotcationConstants".clazz ?: return
+        val target = "com.tencent.mobileqq.text.EmotcationConstants".toClass
         target.findMethod { name = "getSingleEmoji" }.returnConstant(this, -1)
         target.findMethod { name = "getDoubleEmoji" }.returnConstant(this, -1)
     }
