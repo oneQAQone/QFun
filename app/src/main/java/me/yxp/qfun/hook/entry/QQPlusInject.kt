9a2b0ae9b1e5d7b23e1b639daf11135a8c64f7ea
空白用户 <239538389+kongbaiwang639-ai@users.noreply.github.com>
2026-04-13 13:29:44 +0800

    Enhance QQPlusInject with storage clean feature
    Added a storage clean option to the QQPlusInject menu.

--- app/src/main/java/me/yxp/qfun/hook/entry/QQPlusInject.kt
+++ app/src/main/java/me/yxp/qfun/hook/entry/QQPlusInject.kt
@@ -7,10 +7,12 @@ import com.tencent.widget.PopupMenuDialog.OnClickActionListener
 import me.yxp.qfun.R
 import me.yxp.qfun.activity.PluginActivity
 import me.yxp.qfun.activity.SettingActivity
+import me.yxp.qfun.activity.StorageCleanActivity
 import me.yxp.qfun.annotation.HookItemAnnotation
 import me.yxp.qfun.hook.base.BaseApiHookItem
 import me.yxp.qfun.hook.base.Listener
 import me.yxp.qfun.utils.hook.hookBefore
+import me.yxp.qfun.utils.qq.HostInfo
 import me.yxp.qfun.utils.qq.QQCurrentEnv
 import me.yxp.qfun.utils.reflect.findMethod
 
@@ -19,6 +21,15 @@ object QQPlusInject : BaseApiHookItem<Listener>() {
 
     @Suppress("UNCHECKED_CAST")
     override fun loadHook() {
+        val deleteIconRes = try {
+            HostInfo.hostContext.resources.getIdentifier(
+                "qui_delete_oversized",
+                "drawable",
+                HostInfo.packageName
+            )
+        } catch (e: Exception) {
+            R.drawable.ic_launcher
+        }
 
         PopupMenuDialog::class.java
             .findMethod {
@@ -47,34 +58,34 @@ object QQPlusInject : BaseApiHookItem<Listener>() {
                             R.drawable.ic_launcher
                         )
                     )
+                    // 在原有两项之后添加垃圾清理（此时前两项索引为0和1，新项索引2）
+                    add(
+                        2,
+                        MenuItem(
+                            R.string.storage_clean,
+                            "垃圾清理",
+                            "StorageClean",
+                            deleteIconRes
+                        )
+                    )
                 }
 
                 val origin = param.args[2] as OnClickActionListener
 
                 param.args[2] = OnClickActionListener { menuItem ->
-
                     when (menuItem.id) {
                         R.string.app_name -> activity.startActivity(
-                            Intent(
-                                activity,
-                                SettingActivity::class.java
-                            )
+                            Intent(activity, SettingActivity::class.java)
                         )
-
                         R.string.plugin_name -> activity.startActivity(
-                            Intent(
-                                activity,
-                                PluginActivity::class.java
-                            )
+                            Intent(activity, PluginActivity::class.java)
                         )
-
+                        R.string.storage_clean -> activity.startActivity(
+                            Intent(activity, StorageCleanActivity::class.java)
+                        )
                         else -> origin.onClickAction(menuItem)
                     }
-
                 }
-
             }
-
     }
-
 }
