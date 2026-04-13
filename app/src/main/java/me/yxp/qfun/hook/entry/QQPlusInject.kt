ff7fd03ecd4736c740f1cbbba0dede2f1e8c9eef
空白用户 <239538389+kongbaiwang639-ai@users.noreply.github.com>
2026-04-13 10:04:38 +0800

    Update QQPlusInject to modify menu items
    Remove existing menu items and add new ones for QFun, JavaPlugin, and StorageClean.

--- app/src/main/java/me/yxp/qfun/hook/entry/QQPlusInject.kt
+++ app/src/main/java/me/yxp/qfun/hook/entry/QQPlusInject.kt
@@ -7,10 +7,12 @@ import com.tencent.widget.PopupMenuDialog.OnClickActionListener
 import me.yxp.qfun.R
 import me.yxp.qfun.activity.PluginActivity
 import me.yxp.qfun.activity.SettingActivity
+import me.yxp.qfun.hook.device.StorageCleanActivity
 import me.yxp.qfun.annotation.HookItemAnnotation
 import me.yxp.qfun.hook.base.BaseApiHookItem
 import me.yxp.qfun.hook.base.Listener
 import me.yxp.qfun.utils.hook.hookBefore
+import me.yxp.qfun.utils.qq.HostInfo
 import me.yxp.qfun.utils.qq.QQCurrentEnv
 import me.yxp.qfun.utils.reflect.findMethod
 
@@ -19,6 +21,16 @@ object QQPlusInject : BaseApiHookItem<Listener>() {
 
     @Suppress("UNCHECKED_CAST")
     override fun loadHook() {
+        // 获取 QQ 自带的删除图标
+        val deleteIconRes = try {
+            HostInfo.hostContext.resources.getIdentifier(
+                "qui_delete_oversized",
+                "drawable",
+                HostInfo.packageName
+            )
+        } catch (e: Exception) {
+            R.drawable.ic_launcher
+        }
 
         PopupMenuDialog::class.java
             .findMethod {
@@ -28,53 +40,53 @@ object QQPlusInject : BaseApiHookItem<Listener>() {
                 val activity = QQCurrentEnv.activity ?: return@hookBefore
                 val menuItemList = param.args[1] as MutableList<MenuItem>
 
-                menuItemList.apply {
-                    add(
-                        0,
-                        MenuItem(
-                            R.string.plugin_name,
-                            "JavaPlugin",
-                            "JavaPlugin",
-                            R.drawable.ic_float_ball
-                        )
-                    )
-                    add(
-                        0,
-                        MenuItem(
-                            R.string.app_name,
-                            "QFun",
-                            "QFun",
-                            R.drawable.ic_launcher
-                        )
-                    )
+                val toRemove = menuItemList.filter {
+                    it.id == R.string.app_name || 
+                    it.id == R.string.plugin_name || 
+                    it.id == R.string.storage_clean
                 }
+                menuItemList.removeAll(toRemove)
+
+                // 按顺序创建菜单项
+                val qfunItem = MenuItem(
+                    R.string.app_name,
+                    "QFun",
+                    "QFun",
+                    R.drawable.ic_launcher
+                )
+                val pluginItem = MenuItem(
+                    R.string.plugin_name,
+                    "JavaPlugin",
+                    "JavaPlugin",
+                    R.drawable.ic_float_ball
+                )
+                val cleanItem = MenuItem(
+                    R.string.storage_clean,
+                    "缓存清理",
+                    "StorageClean",
+                    deleteIconRes
+                )
+
+                menuItemList.add(0, qfunItem)
+                menuItemList.add(1, pluginItem)
+                menuItemList.add(2, cleanItem)
 
                 val origin = param.args[2] as OnClickActionListener
 
                 param.args[2] = OnClickActionListener { menuItem ->
-
                     when (menuItem.id) {
                         R.string.app_name -> activity.startActivity(
-                            Intent(
-                                activity,
-                                SettingActivity::class.java
-                            )
+                            Intent(activity, SettingActivity::class.java)
                         )
-
                         R.string.plugin_name -> activity.startActivity(
-                            Intent(
-                                activity,
-                                PluginActivity::class.java
-                            )
+                            Intent(activity, PluginActivity::class.java)
                         )
-
+                        R.string.storage_clean -> activity.startActivity(
+                            Intent(activity, StorageCleanActivity::class.java)
+                        )
                         else -> origin.onClickAction(menuItem)
                     }
-
                 }
-
             }
-
     }
-
 }
